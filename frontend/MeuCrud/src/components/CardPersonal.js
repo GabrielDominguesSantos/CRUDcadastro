c426e6ac26659a93c9d4fcbe5912436019ee3c4b
GabrielDominguesSantos <gab.domiingues@gmail.com>
2026-03-17 20:46:04 -0300

    Funcionalidades básicas funcionando apenas usando os códigos disponibilizados

---
+++ frontend/MeuCrud/src/components/CardPersonal.js
@@ -0,0 +1,44 @@
+import { deletePerson } from "../servers/peopleCrud";
+import { View, Text, Button } from "react-native";
+import styles from "../styles/styles";
+
+function CardPersonal({item, navigation, refresh}){
+
+  return(
+
+    <View style={styles.card}>
+
+      <View>
+
+        <Text style={styles.name}>
+          {item.firstName} {item.lastName}
+        </Text>
+
+        <Text style={styles.email}>
+          {item.email}
+        </Text>
+
+      </View>
+
+      <View>
+
+        <Button 
+          title="Editar"
+          onPress={() => navigation.navigate("AddEditScreen", {person: item})}
+        />
+
+        <Button 
+          title="Deletar"
+          onPress={async () => {
+            await deletePerson(item.id);
+            refresh();
+          }}
+        />
+
+      </View>
+
+    </View>
+  )
+}
+
+export default CardPersonal;
---
+++ frontend/MeuCrud/src/screens/HomeScreen.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, Button } from "react-native";
+import CardPersonal from "../components/CardPersonal";
+import styles from "../styles/styles";
+
+import { getPeople, deletePerson } from "../servers/peopleCrud";
+
+export default function HomeScreen({ navigation }) {
+
+  // estado da lista
+  const [people, setPeople] = useState([]);
+
+  // função para carregar dados
+  async function loadPeople(){
+
+    const data = await getPeople();
+
+    setPeople(data);
+  }
+
+  // executa ao abrir tela
+  useEffect(() => {
+    loadPeople();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+
+      <Text style={styles.title}>Pessoas</Text>
+
+      <Button 
+        title="Adicionar Pessoa"
+        onPress={() => navigation.navigate("AddEditScreen")}
+      />
+
+      <FlatList
+        data={people}
+        keyExtractor={(item) => item.id.toString()}
+
+        renderItem={({item}) => (
+          <CardPersonal
+            item={item}
+            navigation={navigation}
+            refresh={loadPeople}
+          />
+        )}
+      />
+
+    </View>
+  );
+}
